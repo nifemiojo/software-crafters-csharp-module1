4604c694da72de3d69144b133c91dabf6b2ffea2
Femi Ojo <femi.ojo@asos.com>
2023-03-30 13:45:26 +0100

    first failing test

--- labs/LeapYear/LeapYear.UnitTests/LeapYearCalculatorShould.cs
+++ labs/LeapYear/LeapYear.UnitTests/LeapYearCalculatorShould.cs
@@ -1,8 +1,17 @@
+using LeapYear;
 using NUnit.Framework;
 
 namespace Tests
 {
+	[TestFixture]
     public class LeapYearCalculatorShould
     {
+	    [Test]
+	    public void ReturnTrue_WhenInputIs4()
+	    {
+		    bool result = LeapYearImpl.IsLeapYear(4);
+
+			Assert.True(result);
+	    }
     }
 }
---
+++ labs/LeapYear/LeapYear/LeapYearImpl.cs
@@ -0,0 +1,10 @@
+namespace LeapYear
+{
+	public class LeapYearImpl
+	{
+		public static bool IsLeapYear(int year)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
